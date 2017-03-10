fac25da69edfea834ad3db73b3763052a7297a09
Alberto Carrascosa <Alberto Carrascosa>
2017-03-10 21:56:34 +0100

    added javascript

--- functions.js
+++ functions.js
@@ -1,131 +1,46 @@
 
 			//Javascript
 
-                     
-
-			function invokeScript(divid)
-                        {
-                            var scriptObj = divid.getElementsByTagName("SCRIPT");
-                            var len = scriptObj.length;
-                            for(var i=0; i<len; i++)
-                            {
-                                var scriptText = scriptObj[i].text;
-                                var scriptFile = scriptObj[i].src;
-                                var scriptTag = document.createElement("SCRIPT");
-                                if ((scriptFile !== null) && (scriptFile !== "")){
-                                    scriptTag.src = scriptFile;
-                                }
-                                scriptTag.text = scriptText;
-                                if (!document.getElementsByTagName("HEAD")[0]) {
-                                    document.createElement("HEAD").appendChild(scriptTag);
-                                }
-                                else {
-                                    document.getElementsByTagName("HEAD")[0].appendChild(scriptTag);
-                                }
-                            }
-                        }
-                        
-                        
-			function nuevaConexion()
-			{
-				var xmlhttp=false;
-				 try {
-					 xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
-				     }
-					catch (e)
-					{
-						try {
-							 xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-						    } 
-						catch (E)
-						{ 
-							xmlhttp = false;
-						}
-					}
-
-				 if (!xmlhttp && typeof XMLHttpRequest!=='undefined')
-				 { 
-					xmlhttp = new XMLHttpRequest();
-				 }
-				 return xmlhttp; 
-
-			}
-			
-			
-			function load_content(di, dat) {
-				//document.getElementById(di).innerHTML='<object type="text/html" data=dat ></object>';
-			}
-
-            
-
-			function Cargar(url,capa)
-			{
-				
-				 var contenido = document.getElementById(capa);
-				 var conexion = nuevaConexion();
-				 conexion.open("GET", url,true);
-				 conexion.onreadystatechange=function()
-				 { 
-					if(conexion.readyState === 4)
-					{
-						contenido.innerHTML = conexion.responseText;
-                                                invokeScript(document.getElementById(capa));
-
-					}
-				}; 
-				conexion.send(null);
-                                
-			} 
-                        
-                         
-
-			function CargarForm(url,capa, valores)
-			{
-				
-				 var contenido = document.getElementById(capa);
-				 conexion = nuevaConexion();
-				 conexion.open("POST", url,true);
-				 conexion.onreadystatechange=function()
-				 { 
-					if(conexion.readyState === 4)
-					{
-						contenido.innerHTML = conexion.responseText;
-                                                invokeScript(document.getElementById(capa));
-					}
-				}; 
-				conexion.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-				conexion.send(valores);
-			}
-
-			function ProcesarForm(formulario, url, capa)
-			{
-				valores="";
-				for (i=0; i<formulario.elements.length;i++)
-				{
-					nombre = formulario.elements[i].name;
-					if (nombre!=="")
-					{
-						if ((formulario.elements[i].type === "radio") && (!formulario.elements[i].checked))
-							;
-						else
-						{
-							valores += formulario.elements[i].name + "=";
-							valores += formulario.elements[i].value + "&";	
-						}
-					}
-				}
-				CargarForm(url,capa,valores);
-			
-
-			}
-
-			
-                        function cargaInicial()
-                        {
-                            
-                            Cargar('menu.html','menu');
-                            Cargar('inicial.html','capa1');
-                        }
-
-
-	
+        function load_Footstool() {
+			$("#content").html('<object style="height:1750px; width:100%;" data="Footstool.html"></object>');
+		}
+		
+		function load_PerfectShield() {
+			$("#content").html('<object style="height:1450px; width:100%;" data="PerfectShield.html"></object>');
+		}
+		
+		function load_Tech() {
+			$("#content").html('<object style="height:1900px; width:100%;" data="Tech.html"></object>');
+		}
+		
+		function load_LedgeTrump() {
+			$("#content").html('<object style="height:1440px; width:100%;" data="LedgeTrump.html"></object>');
+		}
+		
+		function load_PerfectPivot() {
+			$("#content").html('<object style="height:1400px; width:100%;" data="PerfectPivot.html"></object>');
+		}
+		
+		function load_RAR() {
+			$("#content").html('<object style="height:1500px; width:100%;" data="RAR.html"></object>');
+		}
+		
+		function load_BReverse() {
+			$("#content").html('<object style="height:1570px; width:100%;" data="BReverse.html"></object>');
+		}
+		
+		function load_ShieldDrop() {
+			$("#content").html('<object style="height:1400px; width:100%;" data="ShieldDrop.html"></object>');
+		}
+		
+		function load_about_game() {
+			$("#content").html('<object style="height:1100px; width:100%;" data="AboutGame.html"></object>');
+		}
+		
+		function load_about_us() {
+			$("#content").html('<object style="height:1100px; width:100%;" data="AboutUs.html"></object>');
+		}
+		
+		function load_glossary() {
+			$("#content").html('<object style="height:800px; width:100%;" data="Glossary.html"></object>');
+		}
